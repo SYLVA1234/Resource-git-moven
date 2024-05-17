5e1691cc9806e797f3b65b8c52420a9dccef77b5
SYLVA1234 <142400531+SYLVA1234@users.noreply.github.com>
2024-05-17 02:15:45 +0100

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+def print_messages():
+    messages = [
+        "Hello, World!",
+        "Welcome to Python programming.",
+        "This is a simple print function.",
+        "Enjoy coding!"
+    ]
+    
+    for message in messages:
+        print(message)
+
+print_messages()
